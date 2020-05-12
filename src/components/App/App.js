dfbb7310d5a48b1be5da3d34cb62d6de67a5bf85
jmczerwinska <asia.frej@gmail.com>
2020-05-12 20:29:57 +0200

    add header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from '../Header';
 import ShoppingList from '../ShoppingList';
 import ItemForm from '../ItemForm';
 import ClearList from '../ClearList';
@@ -6,9 +7,11 @@ import ShoppingListProvider from '../../context/ShoppingListContext';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <ShoppingListProvider>
+        <Header />
         <ItemForm />
         <ShoppingList />
         <ClearList />
