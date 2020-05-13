ac4c7eb1d4d02b3e8c297a73d70ceb38644673c6
jmczerwinska <asia.frej@gmail.com>
2020-05-13 21:52:36 +0200

    define color variable as an obcject

---
+++ src/components/styledComponents/variable.js
@@ -0,0 +1,8 @@
+export const colors = {
+    primary: '#278180',
+    secondary: '#8e5572',
+    primaryDark: '#1b605e',
+    secondaryDark: '#713d56',
+    white: '#f9f8f4',
+    black: '#1c1d1e'
+}
