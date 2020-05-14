5b963725545fe81c4563b11d51b5de3c71ad9806
jmczerwinska <asia.frej@gmail.com>
2020-05-14 13:07:47 +0200

    index file for import and export styled components

---
+++ src/components/styledComponents/index.js
@@ -0,0 +1,15 @@
+import { Button, InvertButton, ButtonGroup } from './Buttons';
+import Icon from './Icon';
+import Card from './Card';
+import AppHeader from './AppHeader';
+import Layout from './Layout';
+
+export {
+    Button,
+    InvertButton,
+    ButtonGroup,
+    Icon,
+    Card,
+    AppHeader,
+    Layout
+};
