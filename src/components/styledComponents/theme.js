9d2e7fe3d66a48e9c4471d1cee0a7b61208709a2
jmczerwinska <asia.frej@gmail.com>
2020-05-14 12:58:47 +0200

    create styled components theme

---
+++ src/components/styledComponents/theme.js
@@ -0,0 +1,16 @@
+const theme = {
+    colors: {
+        primary: '#278180',
+        secondary: '#8e5572',
+        primaryDark: '#1b605e',
+        secondaryDark: '#713d56',
+        white: '#f9f8f4',
+        black: '#1c1d1e',
+    },
+    fonts: {
+        main: "'Montserrat', sans- serif",
+        title: "'Advent Pro', sans-serif",
+    }
+};
+
+export default theme;
--- src/components/styledComponents/variable.js
+++
@@ -1,8 +0,0 @@
-export const colors = {
-    primary: '#278180',
-    secondary: '#8e5572',
-    primaryDark: '#1b605e',
-    secondaryDark: '#713d56',
-    white: '#f9f8f4',
-    black: '#1c1d1e'
-}
