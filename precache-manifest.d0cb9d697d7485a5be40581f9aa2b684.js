712c6ce27274081e218c3226938f2035f8329afa
jmczerwinska <asia.frej@gmail.com>
2020-05-18 00:15:19 +0200

    Updates

---
+++ precache-manifest.d0cb9d697d7485a5be40581f9aa2b684.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a28fc5cfcbf4e0d626f2a27aeb4c1e",
+    "url": "/shopping-list/index.html"
+  },
+  {
+    "revision": "eef5a2ed64d37ff62a95",
+    "url": "/shopping-list/static/js/2.80ce2b9b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-list/static/js/2.80ce2b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58284fe7ebbde7be54d5",
+    "url": "/shopping-list/static/js/main.23044365.chunk.js"
+  },
+  {
+    "revision": "86563f892b6afbb60368",
+    "url": "/shopping-list/static/js/runtime-main.fe7dd302.js"
+  }
+]);
