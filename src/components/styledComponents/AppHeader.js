aba0a05b636f87850d5673f7278ef1b18dad6e3b
jmczerwinska <asia.frej@gmail.com>
2020-05-18 22:43:01 +0200

    header style

--- src/components/styledComponents/AppHeader.js
+++ src/components/styledComponents/AppHeader.js
@@ -5,13 +5,14 @@ const AppHeader = styled.div`
   font-family: 'Advent Pro', sans-serif;
   color: ${({ theme }) => theme.primary};
   padding: 0.3rem;
+  margin-left: -20px;
 
   h1 {
     margin-top: 0.5rem;
     display: inline-block;
     font-weight: 500;
     @media screen and (max-width: 480px) {
-    font-size: 1.25rem;
+    font-size: 1.5rem;
     }
   }
   
--- src/components/styledComponents/Icon.js
+++ src/components/styledComponents/Icon.js
@@ -1,6 +1,7 @@
 import styled, { css } from 'styled-components';
 
 const Icon = styled.i`
+  font-size: 2em;
   color: ${({ theme }) => theme.primary};
   margin: 5px;
   text-align: center;
