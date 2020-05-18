213d764f1652e3f2dd37820ae9297a0344d68ff7
jmczerwinska <asia.frej@gmail.com>
2020-05-18 22:16:15 +0200

    add gradient to theme colors

--- src/components/styledComponents/theme.js
+++ src/components/styledComponents/theme.js
@@ -7,7 +7,7 @@ const light = {
     bg: '#f9f8f4',
     text: '#1c1d1e',
     grey: '#d6d2d2',
-    gradient: 'linear-gradient(135deg, rgba(242, 246, 248, 1) 0%, rgba(216, 225, 231, 0.89) 22%, rgba(181, 198, 208, 0.89) 51%, rgba(216, 225, 231, 0.89) 56%, rgba(177, 196, 206, 0.89) 100%)'
+    gradient: 'linear-gradient(135deg, rgba(242, 246, 248, 1) 0%, rgba(216, 225, 231, 0.89) 22%, rgba(181, 198, 208, 0.89) 51%, rgba(216, 225, 231, 0.89) 56%, rgba(177, 196, 206, 0.89) 100%)',
 };
 
 const dark = {
@@ -19,8 +19,7 @@ const dark = {
     bg: '#34353a',
     text: '#fff8f0',
     grey: '#707275',
-    gradient: 'linear-gradient(135deg, rgba(76,76,76,1) 0%, rgba(28,28,28,1) 48%, rgba(28,29,30,1) 55%, rgba(52,53,58,1) 100%);'
-
-};
+    gradient: 'linear-gradient(135deg, rgba(76,76,76,1) 0%, rgba(28,28,28,1) 48%, rgba(28,29,30,1) 55%, rgba(52,53,58,1) 100%);',
+}
 
 export { light, dark };
