944bd03582da6bd57d405ce57834f2ff40a05bef
jmczerwinska <asia.frej@gmail.com>
2020-05-18 22:16:47 +0200

    toggle checkbox

--- src/components/styledComponents/Checkbox.js
+++ src/components/styledComponents/Checkbox.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useContext} from 'react';
+import styled, { css, ThemeContext } from 'styled-components';
+import { light } from './theme';
 
 const CustomCheckbox = styled.i`
   font-size:0.9em;
@@ -11,21 +12,60 @@ const CustomCheckbox = styled.i`
   border-radius: 4px;
 `;
 
+const Slider = styled.i`
+  position: absolute;
+  top: -1px;
+  left: -1px;
+  font-size: 0.7em;
+  text-align: center;
+  line-height: 14px;
+  padding: 0.12em;
+  font-weight: 900;
+  height: 14px;
+  width: 14px;
+  border-radius: 50%;
+  background-color: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.secondary};
+  transition: all .3s linear;
+  @media screen and (max-width: 480px) {
+    height: 12px;
+    width: 12px;
+    line-height: 12px;
+    font-size: 0.5rem;
+  }
+`;
+
+const toggleLabelStyle = css`
+  position: absolute;
+  top: 2px;
+  right: 2px;
+  width: 30px;
+  height: 16px;
+  border: 2px solid ${({ theme }) => theme.primary};
+  background-color: ${({ theme }) => theme.primary};
+  border-radius: 1em;
+
+  @media screen and (max-width: 480px) {
+    height:12px;
+    width: 24px;
+    top: -2px;
+    right: -2px;
+  }
+`;
+
 const Label = styled.label.attrs(({ checkId }) => ({
   htmlFor: checkId || 'checkbox'
 }))`
   position: relative;
   margin: 0.5em;
   display: inline-block;
+  color: ${({ theme })=> theme.text};
+
+  ${({ toggle }) => toggle && toggleLabelStyle}
 `;
 
-const Checkbox = styled.input.attrs(({checkId}) => ({
-  type: 'checkbox',
-  id: checkId || 'checkbox'
-}))`
-  position: absolute;
-  opacity: 0;
-  &:checked + ${Label} {
+const basicCheckedStyle = css`
+  :checked + ${Label} {
     color: ${({ theme }) => theme.grey};
     text-decoration: line-through;
     ${CustomCheckbox} {
@@ -34,13 +74,40 @@ const Checkbox = styled.input.attrs(({checkId}) => ({
   }
 `;
 
+const toggleCheckedStyle = css`
+  :checked + ${Label} {
+    ${Slider} {
+      transform: translateX(14px); 
+      @media screen and (max-width: 480px){
+        transform: translateX(12px);
+      }   
+    }
+  }
+`;
+
+const Checkbox = styled.input.attrs(({ checkId }) => ({
+  type: 'checkbox',
+  id: checkId || 'checkbox'
+}))`
+  position: absolute;
+  opacity: 0;
+  
+  ${({ basic }) => basic && basicCheckedStyle}
+
+  ${({ toggle }) => toggle && toggleCheckedStyle} 
+`;
+
 const StyledCheckbox = ({ label, ...props }) => {
+const theme = useContext(ThemeContext);
+
   return (
     <div>
       <Checkbox {...props} />
       <Label {...props}>
-        <CustomCheckbox className="fas fa-check" />
-        {label}</Label>
+        {props.basic && <CustomCheckbox className="fas fa-check" />}
+        {props.toggle && <Slider className={theme === light ? 'fas fa-sun' : 'fas fa-moon'} />} 
+        {label}
+      </Label>
     </div>
   )
 }
